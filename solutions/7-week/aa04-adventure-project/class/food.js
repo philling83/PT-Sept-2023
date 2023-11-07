6e50edae98dfaa74559db84ea21c3c49aee46172
Philip Ling <66927690+philling83@users.noreply.github.com>
2023-11-06 21:38:40 -0500

    Add Week 7 solutions

---
+++ solutions/7-week/aa04-adventure-project/class/food.js
@@ -0,0 +1,13 @@
+// Create an edible `Food` class that inherits from the `Item` class
+
+const { Item } = require('./item');
+
+class Food extends Item {
+  constructor(name, description) {
+    super(name, description);
+  };
+};
+
+module.exports = {
+  Food,
+};
